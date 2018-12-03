166789de74bdfe2818bc71713b191995c965ba5b
gnehcgnaw <949174604@qq.com>
2018-12-03 16:34:11 +0800

    eureka-server模块修改

--- eureka-server/src/main/java/com/wangc/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/wangc/EurekaServerApplication.java
@@ -1,9 +1,14 @@
-package com.wangc.eurekaserver;
+package com.wangc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @version 2.0
+ * @author gnehcgnaw
+ * @date 2018年11月30日10:08:01
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
