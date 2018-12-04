39423e5f024afce8a58fc90205e4be1fa7620524
gnehcgnaw <949174604@qq.com>
2018-12-04 17:40:36 +0800

    将spring-bus -kafka全部换成spring-bus-rabbitmq

--- config-server/src/main/java/com/wangc/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/wangc/configserver/ConfigServerApplication.java
@@ -4,12 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
--- getway-zuul-proxy/src/main/java/com/wangc/getwayzuulproxy/GetwayZuulProxyApplication.java
+++ getway-zuul-proxy/src/main/java/com/wangc/getwayzuulproxy/GetwayZuulProxyApplication.java
@@ -1,10 +1,15 @@
 package com.wangc.getwayzuulproxy;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 /**
  * ZuulProxy
@@ -20,9 +25,20 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 @EnableZuulProxy
 @SpringCloudApplication
+@EnableDiscoveryClient
 public class GetwayZuulProxyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GetwayZuulProxyApplication.class, args);
+	}
+
+	/**
+	 * 动态路由,从config-server中拉取配置
+	 */
+	@Qualifier("zuulProperties")
+	@RefreshScope
+	@ConfigurationProperties("zuul")
+	public ZuulProperties zuulProperties(){
+		return new ZuulProperties();
 	}
 }
